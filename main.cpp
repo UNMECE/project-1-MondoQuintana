f705fd5c3b2dc13dde8e11d1a7ac3ca5367ddea8
MondoQuintana <mondoquintana@unm.edu>
2024-10-11 20:49:10 -0600

    V0.3 Functions and Loops Working. Errors with equations

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <iostream>
 #define resistor 1000
 #define dt 1e-6
 #define steps 50000
@@ -18,7 +19,15 @@ void voltage_current(Capacitor *a, int i)
     //equation for current
     a->current[i+1]=a->current[i]-((a->current[i]/(resistor*capacitor))*dt);
     //increment time
-    a->time[i+1] += dt;
+    a->time[i+1] += a->time[i]+dt;
+    if (i % 200 == 0)
+    {
+        std::cout<<"At step "<<i<<", time is "<<a->time[i]<<", current is "<<a->current[i]<<", and voltage is "<<a->voltage[i]<<"."<<std::endl;
+    }
+    else if (i == steps-1)
+    {
+        std::cout<<"At step "<<i+1<<", time is "<<a->time[steps-1]<<", current is "<<a->current[steps-1]<<", and voltage is "<<a->voltage[steps-1]<<"."<<std::endl;
+    }
 }
 
 int main()
@@ -29,7 +38,7 @@ int main()
     a->current = (double*)malloc(50000*sizeof(double));
 
     a->time[0] = 0;
-    a->current[0] = I_T;
+    a->current[0] = V_0/resistor;
     a->voltage[0] = 0;
 
     for (int i = 0; i < steps; i++)
